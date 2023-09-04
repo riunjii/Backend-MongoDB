db4023e6ac384a23e92e2d25ef3727a10950056d
riunjii <vitor2002_souza@hotmail.com>
2023-09-04 12:14:43 -0300

    Segundo Commit

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const app = express()
+const port = 4000
+
+app.use(express.json()) //irá fazer o parse de arquivos json
+//rotas de conteudo publico
+app.use('/', express.static('public'))
+//rotas de api
+app.get('/api',(req,res) => {
+    res.status(200).json({
+            message: 'API Fatec 100% funcional👍',
+            version: '1.0.0'
+    })
+})
+//rota de excessão deve ser a ULTIMA!!!
+app.use(function(req,res){
+    res.status(404).json({
+        errors:[{
+            value:'${req.originalUrl}',
+            msg: 'A rota ${req.orginialUrl} não existe nesta API!',
+            param: 'invalid route'
+        }]
+    })
+})
+
+app.listen(port, function(){
+    console.log('💻Servidor rodando na porta ${port}')
+})
